77ec5d920d63f94574b3af6cdfaac81d10095158
Bagicode <bagicode@gmail.com>
2017-08-12 06:49:16 +0700

    Initial commit

---
+++ app/src/main/java/com/bagicode/www/bagitransisi/MainActivity.java
@@ -0,0 +1,36 @@
+package com.bagicode.www.bagitransisi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.transitionseverywhere.TransitionManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ViewGroup transitionsContainer = (ViewGroup) findViewById(R.id.transitions_container);
+        final TextView text = (TextView) transitionsContainer.findViewById(R.id.text);
+        final Button button = (Button) transitionsContainer.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            boolean visible;
+
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(transitionsContainer);
+                visible = !visible;
+                text.setVisibility(visible ? View.VISIBLE : View.GONE);
+            }
+
+        });
+    }
+}
